6222cf3f8d565a0cbfb08766293e9b2ded19fca1
WaLid-Jbir <anawalid737@gmail.com>
2024-05-26 02:27:36 +0100

    Connect app to mongodb + handle create book

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
-import express from "express";
-const PORT = 5555;
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from'mongoose';
+import { Book } from './models/bookModel.js';
+dotenv.config();
+
+const PORT = 5555 || process.env.PORT;
 
 const app = express();
 
@@ -7,6 +12,33 @@ app.get('/', (req, res) => {
     res.send("Hello Woeld!");
 });
 
-app.listen( PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+// Handles the creation of a new book in the system
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      res
+        .status(400)
+        .send({ message: "Please provide all the required fields" });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+}).then(() => {
+    app.listen( PORT, () => {
+        console.log(`App is listening to port: ${PORT}`);
+    });
+}).catch(err => {
+    console.log(err);
 });
